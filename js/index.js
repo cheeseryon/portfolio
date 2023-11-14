b1bbfbb468c0974520a9fd808533ef6f205113aa
lhb <ldotoroo@naver.com>
2023-11-14 15:58:52 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -27,9 +27,10 @@ function btnClickPageMove () {
 btnClickPageMove()
 
 
-/* PC용 스크롤시 섹션이동 기능 */
-if(window.matchMedia("(min-Width:680px)").matches) {
+/* 스크롤시 섹션이동 기능 */
 let scrolling = true;
+
+/* pc용 스크롤 이벤트 */
 function scrollPageMove(e) {
 	if(scrolling) {
 		if(e.deltaY > 0){
@@ -59,7 +60,48 @@ function scrollPageMove(e) {
 		}, 600)
 	}
 }
+
+/* 모바일용 터치 이벤트 */
+let startScreenY;
+let endScreenY;
+
+function touchEvent () {
+	document.body.addEventListener('touchstart', (e) => {
+	startScreenY = e.changedTouches[0].clientY;
+	});
+	document.body.addEventListener('touchend', (e) => {
+		endScreenY = e.changedTouches[0].clientY;
+		if(scrolling) {
+			if(startScreenY > endScreenY + 100) {
+				if(index < pageSection.length - 1) {
+					index ++
+				}
+			} else if (startScreenY + 100 < endScreenY) {
+				if(index > 0)
+				index --
+			}
+
+			let filteredPage = pageSection.filter((item , idx) => idx < index)
+			let totalHieght = filteredPage.reduce((accumulator,currentHeight) => {
+				return accumulator + currentHeight.clientHeight
+			}, 0)
+
+			window.scrollTo({top: totalHieght , behavior : "smooth"}); 
+
+			for(let i = 0; i < pageSection.length; i ++) {
+				pageSection[i].classList.remove('active')
+			}
+			pageSection[index].classList.add('active')
+
+			scrolling = false;
+			setTimeout(() => {
+				scrolling = true
+			}, 600)
+		}
+		btnClassAdd()
+	});
 }
+touchEvent()
 
 /* 버튼에 active 클래스 추가 함수 */
 function btnClassAdd() {
