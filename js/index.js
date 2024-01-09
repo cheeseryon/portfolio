5f25a3caf0feba3471f055a75e2816c2b8783fbe
lhb <ldotoroo@naver.com>
2024-01-09 20:06:29 +0900

    전체 재작업

--- js/index.js
+++ js/index.js
@@ -1,37 +1,93 @@
+/* page height 적용 */
+let pageSection = Array.from(document.querySelectorAll('.page'))
+function sectionHeightCalc() {
+	pageSection.forEach((item) => {
+		item.style.height = window.innerHeight + "px"
+	})
+}
+sectionHeightCalc()
+
+function imgAreaHeightCalc() {
+	let imgArea = document.querySelectorAll('.imgArea')
+	if (window.matchMedia("(min-width: 380px) and (max-width: 800px)").matches) {
+			imgArea.forEach((item) => {
+				item.style.height = item.clientWidth * 0.75 + "px"
+			})
+		}
+}
+
+/* 첫 렌더링시에만 home 화면의 title 변경 이벤트 */
+function fistRenderingHomePageTitleChange () {
+	let homePage = document.querySelector('#home')
+	if(homePage.classList.contains("active")) {
+		homePage.classList.add("change")
+	}
+	return
+}
+fistRenderingHomePageTitleChange()
+
 
 /* 스파이스크롤 기능 */
-let pageMoveBtn = Array.from(document.querySelectorAll('.pageMoveBtn'))
-let pageSection = Array.from(document.querySelectorAll('section'))
+let asideMoveBtn = Array.from(document.querySelectorAll('.asideMoveBtn'))
+let menuMoveBtn = Array.from(document.querySelectorAll('.menuMoveBtn'))
 let index = 0
-function btnClickPageMove () {
-	for(let i = 0; i < pageMoveBtn.length; i++) {
-		pageMoveBtn[i].addEventListener('click' , function(e) {
-			e.preventDefault()
-			index = e.target.getAttribute('data-btnIdx')
 
-			let filteredPage = pageSection.filter((item,idx) => idx < index)
-			let totalHeight = filteredPage.reduce((accumulator,currentHeight) => {
-				return accumulator + currentHeight.clientHeight
-			}, 0)
+function OnClickPageMove (moveBtn) {
+	this.btn = moveBtn;
+	this.func = function () {
+		for(let i = 0; i < this.btn.length; i++) {
+			this.btn[i].addEventListener('click' , function(e) {
+				e.preventDefault()
+				index = this.getAttribute('data-btnIdx')
 
-			window.scrollTo({top: totalHeight , behavior : "smooth"}); 
+				let filteredPage = pageSection.filter((item,idx) => idx < index)
+				let totalHeight = filteredPage.reduce((accumulator,currentHeight) => {
+					return accumulator + currentHeight.clientHeight
+				}, 0)
 
-			for(let i = 0; i < pageSection.length; i ++ ){
-				pageSection[i].classList.remove('active')
-			}
-			pageSection[index].classList.add('active')
+				window.scrollTo({top: totalHeight , behavior:"smooth"}); 
 
-			btnClassAdd()//버튼 클릭시 active 클래스 추가를 위한 함수
-		})
+				for(let i = 0; i < pageSection.length; i ++ ){
+					pageSection[i].classList.remove('active')
+				}
+				pageSection[index].classList.add('active')
+
+				addActiveClass()
+				scrollToTopIconShow()	
+				fistRenderingHomePageTitleChange()			
+			})
+		}
 	}
 }
-btnClickPageMove()
+
+let asideBtnOnClick = new OnClickPageMove(asideMoveBtn)
+let fullMenuBtnOnClick = new OnClickPageMove(menuMoveBtn)
+
+asideBtnOnClick.func()
+fullMenuBtnOnClick.func()
 
 
-/* 스크롤시 섹션이동 기능 */
+/* 메뉴창 보이기 */
+function onClickFullMenuShow () {
+	let fullMenuShowBtn = document.querySelector('#fullMenuBtn > a')
+	let fullMenu = document.querySelector('#fullMenu')
+
+	fullMenuShowBtn.addEventListener("click" , function(e) {
+		e.preventDefault()
+		 	if(this.classList.contains("active")) {
+				this.classList.remove("active") 
+				fullMenu.classList.remove("active") 
+			} else {
+				this.classList.add("active") 
+				fullMenu.classList.add("active") 
+			}
+	})
+}
+onClickFullMenuShow()
+
+
+/* 스크롤시 페이지 이동 */
 let scrolling = true;
-
-/* pc용 스크롤 이벤트 */
 function scrollPageMove(e) {
 	if(scrolling) {
 		if(e.deltaY > 0){
@@ -44,11 +100,11 @@ function scrollPageMove(e) {
 		}
 
 		let filteredPage = pageSection.filter((item , idx) => idx < index)
-		let totalHieght = filteredPage.reduce((accumulator,currentHeight) => {
+		let totalHeight = filteredPage.reduce((accumulator,currentHeight) => {
 			return accumulator + currentHeight.clientHeight
 		}, 0)
 
-		window.scrollTo({top: totalHieght , behavior : "smooth"}); 
+		window.scrollTo({top: totalHeight , behavior : "smooth"}); 
 
 		for(let i = 0; i < pageSection.length; i ++) {
 			pageSection[i].classList.remove('active')
@@ -60,60 +116,97 @@ function scrollPageMove(e) {
 			scrolling = true
 		}, 600)
 	}
+
+	scrollToTopIconShow()
+	fistRenderingHomePageTitleChange()
 }
 
-/* 버튼에 active 클래스 추가 함수 */
-function btnClassAdd() {
-	for(let i = 0; i < pageMoveBtn.length; i++) {
+
+/* 스파이 스크롤 버튼에 active 클래스 추가하기*/
+function addActiveClass() {
+	for(let i = 0; i < asideMoveBtn.length; i++) {
 		if(pageSection[i].classList.contains('active')) {
-			pageMoveBtn[i].classList.add('active')
+			asideMoveBtn[i].classList.add('active')
+			menuMoveBtn[i].classList.add('active')
 		} else {
-			pageMoveBtn[i].classList.remove('active')
+			asideMoveBtn[i].classList.remove('active')
+			menuMoveBtn[i].classList.remove('active')
 		}
 	} 
 }
 
+
 /* 새로고침시 버튼에 active클래스 추가 */
-function btnReloadClassAdd() {
+function reloadAddActiveClass() {
 	let filteredPage = pageSection.filter((item)=> 
 		item.getBoundingClientRect().top <= 0 && item.getBoundingClientRect().bottom > 0
 	)
-	let pageIndex = filteredPage[0].getAttribute('data-pageIdx')
-	index = pageIndex
+	index = filteredPage[0].getAttribute('data-pageIdx')
 
 	pageSection[index].classList.add('active')
-	btnClassAdd()
+	addActiveClass()
+	fistRenderingHomePageTitleChange()
 }
 
 
-/* 모바일 gnb 보이기 */
-let moblieGnbShowBtn = document.querySelector('.moblieGnb a img')
-function mobileGnbShow() {
-	moblieGnbShowBtn.addEventListener('click' , function() {
-		let leftArea = document.querySelector('nav')
-		leftArea.classList.add('active')
+/* top 이동 아이콘 show && hide */
+function scrollToTopIconShow() {
+	let elem = document.querySelectorAll("#scroll div")
+	let pageLastNum = pageSection.length - 1
+
+	if(pageSection[pageLastNum].classList.contains("active")) {
+		elem[0].classList.add("hide")
+		elem[0].classList.remove("show")
+		elem[1].classList.add("show")
+		elem[1].classList.remove("hide")
+	} else {
+		elem[0].classList.remove("hide")
+		elem[0].classList.add("show")
+		elem[1].classList.remove("show")
+		elem[1].classList.add("hide")
+	}
+}
+scrollToTopIconShow()
+
+
+/* 맨위로 이동 */
+function scrollToTop(e) {
+	e.preventDefault()
+	index = 0;
+	window.scrollTo({top:0, behavior:"smooth"})
+	for(let i = 0; i < pageSection.length; i++) {
+		pageSection[i].classList.remove("active")
+	}
+	pageSection[0].classList.add("active")
+}
+
+function onClickScrollTopBtn () {
+	document.querySelector(".scrollTop > a").addEventListener("click", (e) => {
+		scrollToTop(e)
+		addActiveClass()
+		scrollToTopIconShow()
 	})
 }
-mobileGnbShow()
+onClickScrollTopBtn()
 
 
-/* 모바일 gnb 숨기기 */
-let mobileGnbCloseBtn = document.querySelector('.closeBtn a img')
-function mobileGnbClose() {
-	mobileGnbCloseBtn.addEventListener('click' , function() {
-		let leftArea = document.querySelector('nav')
-		leftArea.classList.remove('active')
-	})
-}
-mobileGnbClose()
 
+/* window 이벤트 */
 window.addEventListener('wheel', function(e) {
 		e.preventDefault()
 		scrollPageMove(e)
-		btnClassAdd()
+		addActiveClass()
+		
 	}, {passive: false}
 )
 window.addEventListener('load' , function () {
-	btnReloadClassAdd()
+	reloadAddActiveClass()
   }
 ) 
+
+window.addEventListener('resize' , function () {
+	sectionHeightCalc()
+	imgAreaHeightCalc()
+  }
+) 
+
